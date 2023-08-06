ec813f964d41282a5dd51d27cf6c6d7a0f6704cd
Connor Chevli <connorchev@gmail.com>
2023-08-06 19:49:00 -0400

    fix: wip

---
+++ packages/eslint/index.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
