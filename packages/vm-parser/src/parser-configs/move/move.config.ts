e73704fb77b7229bfa77373fef442bbe4237304e
Connor Chevli <connorchev@gmail.com>
2023-08-07 00:16:28 -0400

    fix: init first parser

---
+++ packages/vm-parser/src/parser-configs/move/move.config.ts
@@ -0,0 +1,52 @@
+import { UriDestination, type CreateParserConfig } from '@/types/parser-config.js';
+import { fetcher } from '@/utils/fetcher.js';
+import type { MoveTxDetails } from './types/transaction-details.js';
+
+export const createMoveVmConfig: CreateParserConfig<{
+  transactionDetails: (txId: string) => Promise<MoveTxDetails>;
+}> = (rpcUrl: string) => {
+  const moveVmClient = async <TResponse = unknown>(method: string, params: any[]) => {
+    return await fetcher<TResponse>(rpcUrl, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;',
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        id: 1,
+        method,
+        params,
+      }),
+    });
+  };
+
+  return {
+    transactionDetails: {
+      getPageData: async (txId) => {
+        const { result } = await moveVmClient<any>('getTransactionByHash', [
+          {
+            data: txId,
+          },
+        ]);
+
+        return result;
+      },
+      displayFormat: (tx) => {
+        return [
+          {
+            cardtitle: 'Transaction Details',
+            fields: [
+              {
+                name: 'Tx Hash',
+                description: 'The hash of the transaction.',
+                displayValue: tx.data.hash,
+                uriDestination: UriDestination.ADDRESS,
+                uriValue: tx.data.hash,
+              },
+            ],
+          },
+        ];
+      },
+    },
+  };
+};
--- packages/vm-parser/src/types/parser-config.ts
+++ packages/vm-parser/src/types/parser-config.ts
@@ -1,10 +1,58 @@
-export type CreateParserConfig = (rpcUrl: string) => ParserConfig;
+import type { MaybeArray } from './type-utils.js';
 
-export type ParserConfig = {
-  getTransactionDetails: (txId: string) => Promise<TransactionDetails>;
+export type CreateParserConfig<T extends PageFetchers> = (rpcUrl: string) => ParserConfig<T>;
+
+export type PageFetchers = {
+  transactionDetails?: (txId: string) => Promise<any>;
+  addressDetails?: (address: string) => Promise<any>;
 };
 
-type TransactionDetails = {
-  txId: string;
-  blockHash: string;
+type DataFetcherFunc = (...args: any[]) => Promise<any>;
+
+export type ParserConfig<T extends PageFetchers> = {
+  [K in keyof T]?: T[K] extends DataFetcherFunc ? PageConfig<T[K]> : never;
 };
+
+type PageConfig<TDataFetcherFunc extends DataFetcherFunc> = {
+  getPageData: TDataFetcherFunc;
+  displayFormat: (data: Awaited<ReturnType<TDataFetcherFunc>>) => CardSection[];
+};
+
+export type CardSection = {
+  cardtitle?: string;
+  fields: Field[];
+};
+
+export type Field = {
+  // The name of the field.
+  name: string;
+
+  // The tooltip description for the datum.
+  description?: string;
+
+  // The value to be displayed in the explorer.
+  displayValue: MaybeArray<string | number | boolean>;
+
+  // The value that will be used to link to another page in the explorer
+  uriValue: string | number;
+
+  // The page that the link will point to. If not provided, the value won't link anywhere.
+  uriDestination: UriDestination;
+};
+
+export enum UriDestination {
+  ADDRESS = 'address',
+  BLOCK_HASH = 'block-hash',
+  BLOCK_NUMBER = 'block-number',
+  TRANSACTION_HASH = 'transaction-hash',
+}
+
+export enum DataDisplayType {
+  HASH = 'hash',
+  NUMBER = 'number',
+  STRING = 'string',
+  BOOLEAN = 'boolean',
+  SUCCESS_FAILURE = 'success-failure',
+  DATETIME = 'datetime',
+  RELATIVE_DATETIME = 'relative-datetime',
+}
