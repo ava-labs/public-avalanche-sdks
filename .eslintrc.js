1e87af57ad4f386dfe525a6a441feef36c583309
Connor Chevli <connorchev@gmail.com>
2023-08-17 03:37:35 -0400

    fix: rename internal config packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `@avalabs/eslint`
-  extends: ['@avalabs/eslint'],
-  settings: {
-    next: {
-      rootDir: ['packages/*/'],
-    },
-  },
+  extends: ['custom'],
 };
