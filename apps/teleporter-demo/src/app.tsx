097c74565eb72bb064262c11725c8b84cf901732
Connor Chevli <connorchev@gmail.com>
2023-08-25 00:33:31 -0400

    fix: shadcn theming fully set up

--- apps/teleporter-demo/src/app.tsx
+++ apps/teleporter-demo/src/app.tsx
@@ -1,8 +1,26 @@
+import { TypographyDemo } from './pages/typography-demo';
+import { Button } from './ui/button';
+import { Card, CardContent } from './ui/card';
+import { ThemeProvider } from './ui/theme-provider';
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ThemeProvider
+        defaultTheme="dark"
+        storageKey="teleporter-ui-theme"
+      >
+        <h1 className="text-3xl">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Hello world!</h1>
+        <Card>
+          <CardContent>
+            <Button>Test</Button>
+            <h2 className="font-mono text-3xl">test</h2>
+          </CardContent>
+        </Card>
+        <TypographyDemo />
+      </ThemeProvider>
     </>
   );
 }
