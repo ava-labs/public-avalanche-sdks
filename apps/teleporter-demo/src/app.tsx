aa700d588e4f7e9571d2484db84febe594bd5f82
Connor Chevli <connorchev@gmail.com>
2023-08-25 00:02:11 -0400

    tailwind finally working

--- apps/teleporter-demo/src/app.tsx
+++ apps/teleporter-demo/src/app.tsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-3xl">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
--- apps/teleporter-demo/vite.config.ts
+++ apps/teleporter-demo/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import { dotenvLoad } from 'dotenv-mono';
 import * as path from 'path';
 import bundleVisualizer from 'rollup-plugin-visualizer';
-import removeConsole from 'vite-plugin-remove-console';
 import { Plugin, defineConfig } from 'vite';
 
 /**
@@ -29,55 +28,10 @@ const getBundleVisualizerPlugin: Plugin = () => ({
  * @see https://vitejs.dev/config/
  */
 export default defineConfig(() => ({
-  define: {
-    'process.env': {},
-    'import.meta.env.BUILD_TIMESTAMP': JSON.stringify(new Date()), // timestamp printed in footer for QA
-  },
-  plugins: [
-    removeConsole({ includes: ['log', 'warn', 'table'] }), // remove console.log from production builds, only keep console.error
-    react(),
-    // getBundleVisualizerPlugin(), // uncomment this to analyze bundle
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'esnext',
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-  },
-  build: {
-    target: 'esnext',
-    outDir: './dist',
-    chunkSizeWarningLimit: 5000, // 5000 kbs
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        assetFileNames: ({ name }) => {
-          if (!name) {
-            throw new Error('Missing name in assetFileNames');
-          }
-
-          const fileExt = name.split('.').pop();
-
-          if (fileExt === 'woff' || fileExt === 'woff2') {
-            // removing hash on fonts so they can be preloaded
-            return `assets/[name].[ext]`;
-          }
-
-          return `assets/[name].[hash].[ext]`;
-        },
-      },
     },
   },
 }));
