eb3d67423c07e64d5ac50cae2347da367560f00e
Connor Chevli <connorchev@gmail.com>
2023-08-26 08:30:42 -0400

    fix: add mint-tokens button

--- apps/teleporter-demo/src/components/teleporter-form.tsx
+++ apps/teleporter-demo/src/components/teleporter-form.tsx
@@ -14,6 +14,8 @@ import { ScrollArea } from '@/ui/scroll-area';
 import { useAccount } from 'wagmi';
 import { CardContent } from '@/ui/card';
 import { ConnectWalletButton } from './connect-wallet-button';
+import { Button } from '@/ui/button';
+import { ArrowRight } from 'lucide-react';
 
 const formSchema = z.object({
   fromChain: z.string(),
@@ -45,166 +47,179 @@ export const TeleporterForm = memo(() => {
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8"
-      >
-        <div className="grid grid-cols-12 gap-4">
-          <FormField
-            control={form.control}
-            name="fromChain"
-            render={({ field }) => (
-              <FormItem className="md:col-span-5 col-span-12">
-                <FormLabel>From</FormLabel>
-                <FormControl>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                    disabled={isSubmitting}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a subnet" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {CHAINS.map((chain) => (
-                        <SelectItem
-                          value={chain.chainId}
-                          key={chain.chainId}
-                        >
-                          <div className="flex items-center space-x-2 flex-nowrap">
-                            <FancyAvatar
-                              src={chain.logoUrl}
-                              label={chain.name}
-                              className="w-6 h-6"
-                            />
-                            <p>{chain.name}</p>
-                          </div>
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormDescription>The source chain</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="md:col-span-2 col-span-12 flex justify-center">
-            <SwapButton
-              type="button"
-              disabled={isSubmitting}
-              className="md:mt-8 max-md:rotate-90"
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8"
+        >
+          <div className="grid grid-cols-12 gap-4">
+            <FormField
+              control={form.control}
+              name="fromChain"
+              render={({ field }) => (
+                <FormItem className="md:col-span-5 col-span-12">
+                  <FormLabel>From</FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      disabled={isSubmitting}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a subnet" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {CHAINS.map((chain) => (
+                          <SelectItem
+                            value={chain.chainId}
+                            key={chain.chainId}
+                          >
+                            <div className="flex items-center space-x-2 flex-nowrap">
+                              <FancyAvatar
+                                src={chain.logoUrl}
+                                label={chain.name}
+                                className="w-6 h-6"
+                              />
+                              <p>{chain.name}</p>
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormDescription>The source chain</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="md:col-span-2 col-span-12 flex justify-center">
+              <SwapButton
+                type="button"
+                disabled={isSubmitting}
+                className="md:mt-8 max-md:rotate-90"
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="toChain"
+              render={({ field }) => (
+                <FormItem className="md:col-span-5 col-span-12">
+                  <FormLabel>To</FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      disabled={isSubmitting}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a subnet" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {CHAINS.map((chain) => (
+                          <SelectItem
+                            value={chain.chainId}
+                            key={chain.chainId}
+                          >
+                            <div className="flex items-center space-x-2 flex-nowrap">
+                              <FancyAvatar
+                                src={chain.logoUrl}
+                                label={chain.name}
+                                className="w-6 h-6"
+                              />
+                              <p>{chain.name}</p>
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormDescription>The destination chain</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
           </div>
           <FormField
             control={form.control}
-            name="toChain"
+            name="token"
             render={({ field }) => (
-              <FormItem className="md:col-span-5 col-span-12">
-                <FormLabel>To</FormLabel>
+              <FormItem>
+                <FormLabel>Token</FormLabel>
                 <FormControl>
                   <Select
                     onValueChange={field.onChange}
                     defaultValue={field.value}
                     disabled={isSubmitting}
                   >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a subnet" />
-                      </SelectTrigger>
-                    </FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
                     <SelectContent>
-                      {CHAINS.map((chain) => (
-                        <SelectItem
-                          value={chain.chainId}
-                          key={chain.chainId}
-                        >
-                          <div className="flex items-center space-x-2 flex-nowrap">
-                            <FancyAvatar
-                              src={chain.logoUrl}
-                              label={chain.name}
-                              className="w-6 h-6"
-                            />
-                            <p>{chain.name}</p>
-                          </div>
-                        </SelectItem>
-                      ))}
+                      <ScrollArea className="max-h-72">
+                        <SelectGroup>
+                          {isConnected ? (
+                            tokens?.map((token) => {
+                              return (
+                                <SelectItem
+                                  value={token.universalTokenId}
+                                  key={token.universalTokenId}
+                                  className="h-12 rounded-lg"
+                                >
+                                  <div className="flex items-center space-x-2 flex-nowrap">
+                                    <FancyAvatar
+                                      src={token.logoUri}
+                                      label={token.symbol}
+                                      className="w-6 h-6"
+                                    />
+                                    <p>{token.name}</p>
+                                  </div>
+                                </SelectItem>
+                              );
+                            })
+                          ) : (
+                            <CardContent className="flex flex-col items-center space-y-3">
+                              <p className="text-gray-300 text-center text-sm">
+                                Must connect wallet to view your tokens
+                              </p>
+                              <ConnectWalletButton
+                                variant="secondary"
+                                className="w-full"
+                              />
+                            </CardContent>
+                          )}
+                        </SelectGroup>
+                      </ScrollArea>
                     </SelectContent>
                   </Select>
                 </FormControl>
-                <FormDescription>The destination chain</FormDescription>
+                <FormDescription className="max-md:flex max-md:flex-col">
+                  <span>The token you want to teleport.</span>
+                  <Button variant="link">
+                    Mint tokens
+                    <ArrowRight
+                      size="16"
+                      className="ml-1"
+                    />
+                  </Button>
+                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-        </div>
-        <FormField
-          control={form.control}
-          name="token"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Token</FormLabel>
-              <FormControl>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  disabled={isSubmitting}
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a fruit" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <ScrollArea className="max-h-72">
-                      <SelectGroup>
-                        {isConnected ? (
-                          tokens?.map((token) => {
-                            return (
-                              <SelectItem
-                                value={token.universalTokenId}
-                                key={token.universalTokenId}
-                                className="h-12 rounded-lg"
-                              >
-                                <div className="flex items-center space-x-2 flex-nowrap">
-                                  <FancyAvatar
-                                    src={token.logoUri}
-                                    label={token.symbol}
-                                    className="w-6 h-6"
-                                  />
-                                  <p>{token.name}</p>
-                                </div>
-                              </SelectItem>
-                            );
-                          })
-                        ) : (
-                          <CardContent className="flex flex-col items-center space-y-3">
-                            <p className="text-gray-300 text-center text-sm">Must connect wallet to view your tokens</p>
-                            <ConnectWalletButton
-                              variant="secondary"
-                              className="w-full"
-                            />
-                          </CardContent>
-                        )}
-                      </SelectGroup>
-                    </ScrollArea>
-                  </SelectContent>
-                </Select>
-              </FormControl>
-              <FormDescription>The token you want to teleport.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <LoadingButton
-          type="submit"
-          className="uppercase w-full"
-          isLoading={isSubmitting}
-        >
-          teleport!
-        </LoadingButton>
-      </form>
-    </Form>
+          <LoadingButton
+            type="submit"
+            className="w-full"
+            isLoading={isSubmitting}
+          >
+            TELEPORT!
+          </LoadingButton>
+        </form>
+      </Form>
+    </>
   );
 });
