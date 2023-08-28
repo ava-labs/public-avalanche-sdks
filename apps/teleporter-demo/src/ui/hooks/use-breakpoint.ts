41a68966b5d7ad5bef96cfc1b6630c8ff69967a2
Connor Chevli <connorchev@gmail.com>
2023-08-27 23:46:56 -0400

    useBreakpoint hook

---
+++ apps/teleporter-demo/src/ui/hooks/use-breakpoint.ts
@@ -0,0 +1,22 @@
+import { useMediaQuery } from 'react-responsive';
+// apps/teleporter-demo/tailwind.config.js
+import partialTailwindConfig from '../../../tailwind.config';
+import resolveConfig from 'tailwindcss/resolveConfig';
+
+const fullTailwindConfig = resolveConfig(partialTailwindConfig);
+
+const breakpoints = fullTailwindConfig.theme!.screens!;
+console.log('breakpoints', breakpoints);
+
+export function useBreakpoint<K extends 'sm' | 'md' | 'lg' | 'xl' | '2xl'>(breakpointKey: K) {
+  const bool = useMediaQuery({
+    // @ts-expect-error test
+    query: `(min-width: ${breakpoints[breakpointKey]})`,
+  });
+  const capitalizedKey = breakpointKey[0]!.toUpperCase() + breakpointKey.substring(1);
+  type Key = `is${Capitalize<K>}Up` | `is${Capitalize<K>}Down`;
+  return {
+    [`is${capitalizedKey}Up`]: bool,
+    [`is${capitalizedKey}Down`]: !bool,
+  } as Record<Key, boolean>;
+}
