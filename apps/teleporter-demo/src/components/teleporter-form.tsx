b9168b361ef662e86b415ad3c2ff8bc2abb37bc7
Connor Chevli <connorchev@gmail.com>
2023-08-28 18:26:53 -0400

    change text from Teleporting to bridging

--- apps/teleporter-demo/src/components/teleporter-form.tsx
+++ apps/teleporter-demo/src/components/teleporter-form.tsx
@@ -266,7 +266,7 @@ export const TeleporterForm = memo(() => {
           onClick={handleTeleport}
           isLoading={isSubmitting}
           disabled={!isReadyToTeleport}
-          loadingText="Teleporting..."
+          loadingText="Bridging..."
           tooltipContent={
             !isConnected
               ? 'Please connect your wallet.'
