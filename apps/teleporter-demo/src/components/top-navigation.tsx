fc6074ab8ce5eb2c21634b0d9766efc8e7844bbb
Connor Chevli <connorchev@gmail.com>
2023-08-29 14:29:33 -0400

    fix formatting in read more popover

--- apps/teleporter-demo/src/components/top-navigation.tsx
+++ apps/teleporter-demo/src/components/top-navigation.tsx
@@ -38,7 +38,7 @@ export function TopNavigation() {
             <NavigationMenuTrigger>Read More</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
+                <li className="row-span-4">
                   <NavigationMenuLink
                     asChild
                     className="bg-primary"
