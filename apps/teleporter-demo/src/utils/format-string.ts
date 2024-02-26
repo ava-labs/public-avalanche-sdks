98be85724256c937663802429cc8cc23c9a20a0b
Connor Chevli <connorchev@gmail.com>
2024-02-25 22:32:42 -0500

    bump intl formatter

--- apps/teleporter-demo/src/utils/format-string.ts
+++ apps/teleporter-demo/src/utils/format-string.ts
@@ -1,4 +1,4 @@
-import { formatters } from '@poppinss/intl-formatter';
+import formatters from '@poppinss/intl-formatter';
 
 export const bigToDisplayString = (bigNum?: Big, options?: Intl.NumberFormatOptions): string =>
   bigNum ? formatters.number('en-US', options).format(bigNum.toString() as unknown as number) : '';
