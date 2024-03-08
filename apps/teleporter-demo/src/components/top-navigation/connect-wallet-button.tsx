20397ed2b6ab84093c81b6fa3af972d5765ba6c0
Connor Chevli <connorchev@gmail.com>
2024-03-08 01:01:13 -0500

    loading state for top nav connect button

--- apps/teleporter-demo/src/components/top-navigation/connect-wallet-button.tsx
+++ apps/teleporter-demo/src/components/top-navigation/connect-wallet-button.tsx
@@ -3,11 +3,13 @@ import { Button, type ButtonProps } from '@/ui/button';
 import { useAccount } from 'wagmi';
 import { SettingsMenu } from './settings-menu';
 import { cn } from '@/utils/cn';
-import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useWeb3Modal, useWeb3ModalState } from '@web3modal/wagmi/react';
+import { LoadingSpinner } from '@/ui/loading-spinner';
 
 export const ConnectWalletButton = ({ className, ...rest }: ButtonProps) => {
-  const { address, isConnected } = useAccount();
-  const { open } = useWeb3Modal();
+  const { address, isConnected, isConnecting } = useAccount();
+  const { open: openModal } = useWeb3Modal();
+  const { open: isModalOpen } = useWeb3ModalState();
 
   if (isConnected && address) {
     return <SettingsMenu />;
@@ -17,7 +19,9 @@ export const ConnectWalletButton = ({ className, ...rest }: ButtonProps) => {
     <Button
       variant="outline"
       className={cn('rounded-full', className)}
-      onClick={() => open()}
+      onClick={() => openModal()}
+      startIcon={(isModalOpen || isConnecting) && <LoadingSpinner />}
+      disabled={isModalOpen || isConnecting}
       {...rest}
     >
       Connect Wallet
