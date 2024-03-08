e1549748f67ffa3ad7254f94ed68281ebbeef071
Connor Chevli <connorchev@gmail.com>
2024-03-08 03:33:42 -0500

    better loading page

--- apps/teleporter-demo/src/pages/loading-page.tsx
+++ apps/teleporter-demo/src/pages/loading-page.tsx
@@ -1,8 +1,17 @@
 import { Progress } from '@/ui/progress';
+import { cn } from '@/utils/cn';
 import { memo } from 'react';
 
 export const LoadingPage = memo(() => {
-  console.log('test');
-
-  return <Progress value={100} />;
+  return (
+    <div className="p-6">
+      <Progress
+        value={100}
+        className="bg-transparent"
+        indicatorProps={{
+          className: cn('animate-pulse'),
+        }}
+      />
+    </div>
+  );
 });
